23898e2689824375dbbe42364e4613948fa7ecd0
Vilcya Wirantana <vwirantana@gmail.com>
2013-08-25 14:05:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acmhacks::Application.config.secret_token = '938a873f9264fc80b527b68553f004ba0f78e509038e3bbf447ac86ef4c7967310ae87241a9c3992c83ec627f62614de81cfa76e0b77d5a5df428cf20a9f20c1'
